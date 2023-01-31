9e270b323139f54851efce2f827903511015a87e
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-01-31 16:42:20 +0100

    fix bug  print

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(9):
-    for j in range(i+1, 10):
-        if i == 8 and j == 9:
-            print("{}".format(i*10+j))
-        else:
-            print("{:02d}".format(i*10+j), end=", ")
+for i in range(0,8):
+    for j in range(i,10):
+        if i != j:
+         print("{:d}{:d}, ".format(i,j), end=" ")
+    print("89")
