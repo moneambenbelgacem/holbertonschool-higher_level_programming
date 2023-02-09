6d5635864cdcabd063a692219d4dd8933beb882e
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-09 12:03:15 +0100

    affiche matrice

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row  in range(len(matrix)):
+    for row in range(len(matrix)):
         for col in range(len(matrix[row])):
             if col != 0:
                 print(" ", end='')
