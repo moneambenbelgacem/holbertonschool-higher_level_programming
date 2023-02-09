a23e101707477cb7fbaf184494d3fce6e8752d2a
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-09 13:10:54 +0100

    defmax

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if my_list == []:
         return None
     max = my_list[0]
-    for i in range(len(my_list)):
+    for i in my_list:
         if i > max:
             max = i
     return max
