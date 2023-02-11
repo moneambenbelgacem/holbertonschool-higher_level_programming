ccd0b028856febfa95f9be1cdf8efe69aa55b200
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-11 14:47:54 +0100

    square with size

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,5 +9,7 @@ class Square:
             self.__size = size
             if size < 0:
                 raise ValueError ("size must be >= 0")
-        except TypeError:
-            print("size must be an integer")
+            elif not isinstance(size, int):
+                raise TypeError ("size must be an integer")
+        except:
+            pass
