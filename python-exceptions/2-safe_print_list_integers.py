95d2411452781ba351dddae5c09c3aea4749f4a6
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-11 12:46:51 +0100

    check type error and value error

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    nbr_print = 0
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end='')
+            nbr_print += 1
+    except TypeError:
+        pass
+    except ValueError:
+        pass
+    print()
+    return nbr_print
