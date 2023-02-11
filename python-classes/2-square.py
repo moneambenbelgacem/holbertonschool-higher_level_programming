d7956105731040f0fd3712717eeee1b5e58f77eb
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-11 15:12:07 +0100

    square with size

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,10 +5,10 @@
 class Square:
     """Fist method"""
     def __init__(self, size=0):
-            if size < 0:
-                raise ValueError ("size must be >= 0")
-            elif not isinstance(size, int):
+            if not isinstance(size, int):
                 raise TypeError ("size must be an integer")
+            elif size < 0:
+                raise ValueError ("size must be >= 0")
             else:
                 self.__size = size
 
