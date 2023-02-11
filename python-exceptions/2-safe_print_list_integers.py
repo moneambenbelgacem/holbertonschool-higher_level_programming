54d3cd2f809d1455d8d1ed69ec59905d1bbfefb2
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-11 12:51:39 +0100

    check type error and value error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     nbr_print = 0
-    try:
-        for i in range(x):
+
+    for i in range(x):
+        try:
             print("{:d}".format(my_list[i]), end='')
             nbr_print += 1
-    except TypeError:
-        pass
-    except ValueError:
-        pass
+        except TypeError:
+            pass
+        except ValueError:
+            pass
     print()
     return nbr_print
