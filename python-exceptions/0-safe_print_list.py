90541051db0d03991a56f9505a7da542e30ced99
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-11 12:34:05 +0100

    fix_betty

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     nbr_print = 0
     try:
         for i in range(x):
-            print(my_list[i],end='')
+            print(my_list[i], end='')
             nbr_print += 1
     except IndexError:
         pass
