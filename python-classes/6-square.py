35b29a990621240f5ba1fa569c2a236c2b857872
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-12 13:08:57 +0100

    ADD position set and prop and all

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -63,5 +63,3 @@ class Square:
         if not all(isinstance(i, int) and i >= 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-
-    
