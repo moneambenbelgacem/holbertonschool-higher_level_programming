0b89460af6f78cefccc365b792cdb2e53c1eac40
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-13 15:27:42 +0100

    say_my_name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module have one function"""
+
+
+def say_my_name(first_name, last_name=""):
+    """function say my name"""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    return f"My name is{first_name}{last_name}"
