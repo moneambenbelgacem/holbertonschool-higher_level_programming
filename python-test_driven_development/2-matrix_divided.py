6e6e33224ed798ace6f6b288958fdf7c8c9daac9
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-13 14:57:47 +0100

    divided matrix

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module have one function"""
+
+def matrix_divided(matrix, div):
+    """divide matrix"""
+    row_len = len(matrix[0])
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for row in matrix[1:]:
+        if len(row) != row_len:
+            raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(item / div, 2) for item in row] for row in matrix]
