18aeaba4f56982308e8f8fc9708c436cc1b37375
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-13 15:28:41 +0100

    say_my_name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -8,4 +8,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    return f"My name is{first_name}{last_name}"
+    return f"My name is{first_name} {last_name}"
