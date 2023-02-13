ee078953ab9019611fcc57b40e111a3bed69ffdb
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-13 14:29:08 +0100

    ADD two numbers

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module have one function"""
+
+
+def add_integer(a, b=98):
+    """add two integer"""
+    if not(isinstance(a, int) or isinstance (a, float)):
+        raise TypeError ("a must be an integer")
+    elif not(isinstance(b, float) or isinstance (b, int)):
+        raise TypeError ("a must be an integer")
+    elif isinstance(a, float):
+        a = int(a)
+    elif isinstance(b, float):
+        b = int(b)
+    else: 
+        return a + b
