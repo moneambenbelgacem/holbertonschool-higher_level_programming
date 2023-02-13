e48bec9c7ed28ebe3bb944d78c76d05e8aa3cd69
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-13 16:00:20 +0100

    split line with conditions

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module have one function"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    new_text = ""
+    for ch in text:
+        if ch in ".?:":
+            new_text += ch + "\n\n"
+        else:
+            new_text += ch
+    lines = new_text.split("\n")
+    for line in lines:
+        if line == lines[-1]:
+            print(line.strip(),end="")
+        else:
+            print(line.strip())
