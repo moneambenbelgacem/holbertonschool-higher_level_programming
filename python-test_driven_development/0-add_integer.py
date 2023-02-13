e454549b50548ffb9d88c38e48b3fdc8d64b7ebb
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-13 14:32:28 +0100

    ADD two numbers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,7 +7,7 @@ def add_integer(a, b=98):
     if not(isinstance(a, int) or isinstance (a, float)):
         raise TypeError ("a must be an integer")
     elif not(isinstance(b, float) or isinstance (b, int)):
-        raise TypeError ("a must be an integer")
+        raise TypeError ("b must be an integer")
     elif isinstance(a, float):
         a = int(a)
     elif isinstance(b, float):
