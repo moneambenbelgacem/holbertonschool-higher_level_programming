da60bcb46856a9268e43ff97ef8a1bde0bfac918
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-14 17:04:35 +0100

    affiche # rect

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -50,3 +50,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__height + self.__width) * 2
+
+    def __str__(self) -> str:
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        return "\n".join("#"*self.width for _ in range(self.height))
