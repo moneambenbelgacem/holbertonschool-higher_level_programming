3597ce68f7e8dff39cce4802e83a18b59a7a1dac
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-14 16:42:08 +0100

    rectangle  width and height

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -19,9 +19,7 @@ class Rectangle:
 
     @property
     def width(self):
-        return self.__width
-
-    
+        return self.__width 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -33,7 +31,6 @@ class Rectangle:
     @property
     def height(self):
         return self.__height
-
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
