3d4ce63a6b07e155b9f8ed62fa2b7798091e7687
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-19 14:24:17 +0100

    methode of value

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -3,7 +3,7 @@
 
 
 class BaseGeometry:
-    """base class"""
+    """base class  """
     def area(self):
         raise Exception("area() is not implemented")
     def integer_validator(self, name, value):
