60c11aa03cf75fbd79d3c87f6cefa583f7b07536
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-19 14:22:23 +0100

    methode of value

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module class rectangle"""
+
+
+class BaseGeometry:
+    """base class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if not isinstance(value, int) or isinstance(value, bool):
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
