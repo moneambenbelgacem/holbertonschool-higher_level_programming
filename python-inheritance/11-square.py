b3c2daadb3c42fcc44b95aa5ea78f14cb7dd8fce
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-22 13:51:43 +0100

    str print

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -15,4 +15,4 @@ class Square(Rectangle):
 
     def __str__(self):
         return "[Square] " + \
-            str(self.__width) + "/" + str(self.__height)
+            str(self.__size) + "/" + str(self.__size)
