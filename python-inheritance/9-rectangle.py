99ae57d0c2a0c3e80d8d0938f03af5bd9666dc78
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-22 10:55:29 +0100

    methode str and area

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -16,4 +16,5 @@ class Rectangle (BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        return "[Rectangle]" + " " + str(self.__width) + "/" + str(self.__height)
+        return "[Rectangle]" + " "\
++ str(self.__width) + "/" + str(self.__height)
