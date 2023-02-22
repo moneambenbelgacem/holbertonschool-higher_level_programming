15e540053734c14e81129cef9876d60c68933690
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-02-22 13:47:19 +0100

    str print

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Module for Rectangle class.'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """rectangle square"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self) -> str:
+        return "[Square] " + \
+            str(self.__width) + "/" + str(self.__height)
