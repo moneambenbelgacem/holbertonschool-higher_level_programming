69038549da4ef8008a40e9d1e7a2180f916dc4fb
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-03-17 21:33:32 +0100

    FULL  create

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,7 +4,7 @@ CREATE TABLE second_table (
    name VARCHAR(50),
    score INT
 );
-INSERT INTO second_table (name, age, email) VALUES 
+INSERT INTO second_table (name, name, score) VALUES 
 (1,"John",10),
 (2,"Alex",3),
 (3,"Bob",14),
