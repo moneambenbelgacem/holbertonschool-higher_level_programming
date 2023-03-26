5800247cfd5d48df0126bc91e9a0cef7cbb5b4cb
monembenbelgacem <moneem.benbelgasem@esen.tn>
2023-03-26 17:27:38 +0100

    inner join

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,2 @@
+-- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+SELECT tv_shows.title AS title FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE  tv_genres.name = "Comedy" ORDER BY title ASC;
