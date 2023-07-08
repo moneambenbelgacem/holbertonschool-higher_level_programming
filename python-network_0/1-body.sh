ffd22190c6bc6272de1b2b8817ac02ecccf8b2e7
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2023-07-08 21:11:59 +0200

    config

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #function
-curl -s -o /dev/null -w "%{http_code}\n" "$1" | grep -q 200 && curl -s "$1"
+curl -s -L -o /dev/null -w "%{http_code}\n" "$1" | grep -q 200 && curl -s -L "$1"
