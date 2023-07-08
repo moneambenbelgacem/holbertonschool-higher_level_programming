49eabd47738d55f396d7b05c9b3afa2ff384f2ca
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2023-07-08 21:15:55 +0200

    config

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 #function
-curl -s -o /dev/null -w "%{http_code}\n" "$1" | grep -q 200 && curl -s "$1"
+response=$(curl -s -L -o /dev/null -w "%{http_code}\n%{redirect_url}" "$1")
+
+redirect_count=$(grep -c "http://" <<< "$response")
+
+if [ "$redirect_count" -eq 1 ]; then
+  curl -s -L "$response"
+fi
