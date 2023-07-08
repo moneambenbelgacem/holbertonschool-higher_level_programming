680089a957032203077a379fa4b0006f8d8d70eb
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2023-07-08 21:07:42 +0200

    config

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 #function
-if [ $# -eq 0 ]; then
-  echo "Usage: ./get_response_body.sh <URL>"
-  exit 1
-fi
-
-url=$1
-
-response=$(curl -s -w "%{http_code}" "$url")
-
-if [ "$response" -eq 200 ]; then
-  curl -s "$url"
-fi
+curl -s -o /dev/null -w "%{http_code}\n" "$1" | grep -q 200 && curl -s "$1"
