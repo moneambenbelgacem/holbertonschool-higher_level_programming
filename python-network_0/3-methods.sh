36e814a94fe13647d39a004bc8f3e12e5dac2b25
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2023-07-08 21:32:08 +0200

    config

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #function
-curl -s -I "$1" | awk '/Allow/ {split($0, methods, ": "); split(methods[2], arr, ", "); for (i in arr) print arr[i]}'
+curl -s -I -X OPTIONS "$1" | awk -F ': ' '/Allow/ {print $2}'
