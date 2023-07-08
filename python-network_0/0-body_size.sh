3bd55118c6b28aa5655b0f83112ea2b7abe0a1fb
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2023-07-08 21:02:43 +0200

    config

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 #function
-if [ $# -eq 0 ]; then
-  echo "Usage: ./get_response_size.sh <URL>"
-  exit 1
-fi
-
-url=$1
-
-response=$(curl -s -w "%{size_download}" -o /dev/null "$url")
-
-echo "Response size: $response bytes"
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
